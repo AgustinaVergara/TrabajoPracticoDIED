95313fa2ed5d8514ea4e700c1f29765ba17532e0
Valentina <ducassevalentina@gmail.com>
2023-07-27 12:13:29 -0300

    creacion de get y set

--- src/main/java/clases/Producto.java
+++ src/main/java/clases/Producto.java
@@ -4,7 +4,31 @@ public class Producto {
 	public String nombre;
 	public String descripcion; 
 	public int precioUnitario;
-	public int pesoKg; 
+	public int pesoKg;
 	
-	public int valor;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getPrecioUnitario() {
+		return precioUnitario;
+	}
+	public void setPrecioUnitario(int precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+	public int getPesoKg() {
+		return pesoKg;
+	}
+	public void setPesoKg(int pesoKg) {
+		this.pesoKg = pesoKg;
+	} 
+	
 }
